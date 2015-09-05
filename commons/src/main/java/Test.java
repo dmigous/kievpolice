6e2eee2deac2eccd53aea229692ac45abb1b2721
koxa29 <koxa29@gmail.com>
2015-09-05 12:45:22 +0300

    added server modules

---
+++ commons/src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by koxa on 05.09.2015.
+ */
+public class Test {
+    public String helloWorld()
+    {
+        return "Hello world";
+    }
+}
---
+++ server/src/main/java/TestService.java
@@ -0,0 +1,8 @@
+/**
+ * Created by koxa on 05.09.2015.
+ */
+public class TestService {
+    public static void main(String[] args) {
+        System.out.println( new Test().helloWorld() );
+    }
+}
