94833ccd3d4cfde4eb03f280a3f56bbb56fcc7c1
Dmitry Belous <dmigous@gmail.com>
2015-09-05 18:58:25 +0300

    app version

--- mobile-apps/android/kievpolice/app/src/main/java/io/github/froger/instamaterial/ui/utils/BackendlessDefaults.java
+++ mobile-apps/android/kievpolice/app/src/main/java/io/github/froger/instamaterial/ui/utils/BackendlessDefaults.java
@@ -7,4 +7,5 @@ public class BackendlessDefaults {
     public static final String BACKENDLESS_HOST = "https://api.backendless.com";
     public static final String APP_ID = "FC98C98C-63A6-F62A-FFAA-A6AC70B80100";
     public static final String SECRET_KEY = "3FCA953B-1251-DE81-FF9A-6F39B5FBAA00";
+    public static final String APP_VERSION = "v1";
 }
