d8b11ddc5481d550de0ff84182d579220253bf62
Dmitry Belous <dmigous@gmail.com>
2015-09-05 20:20:49 +0300

    uploading cropped image

--- mobile-apps/android/kievpolice/app/src/main/java/io/github/froger/instamaterial/ui/activity/PublishActivity.java
+++ mobile-apps/android/kievpolice/app/src/main/java/io/github/froger/instamaterial/ui/activity/PublishActivity.java
@@ -3,9 +3,12 @@ package io.github.froger.instamaterial.ui.activity;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewTreeObserver;
@@ -13,14 +16,23 @@ import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.ToggleButton;
 
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import butterknife.InjectView;
 import butterknife.OnCheckedChanged;
 import hugo.weaving.DebugLog;
 import io.github.froger.instamaterial.R;
 import io.github.froger.instamaterial.Utils;
+import io.github.froger.instamaterial.ui.utils.PoliceService;
 import timber.log.Timber;
 
 /**
@@ -61,6 +73,50 @@ public class PublishActivity extends BaseActivity {
                 ivPhoto.getViewTreeObserver().removeOnPreDrawListener(this);
                 loadThumbnailPhoto();
                 return true;
+            }
+        });
+
+        getToolbar().setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_publish:
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                byte[] image_raw_bytes = new byte[0];
+
+                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                                try {
+                                    Picasso.with(getApplicationContext()).load(photoUri).centerCrop()
+                                            .resize(600, 600).get().compress(Bitmap.CompressFormat.PNG, 100, stream);
+                                    image_raw_bytes = stream.toByteArray();
+
+                                    Log.i("Backend", "Start upload!");
+                                    PoliceService.report("3E0127EB-540B-BD22-FFC5-874985B9B000",
+                                            image_raw_bytes, 0, 0, "test desc");
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                        return true;
+                }
+                return false;
+            }
+
+            public byte[] getBytes(InputStream inputStream) throws IOException {
+                ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+
+                int len = 0;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    byteBuffer.write(buffer, 0, len);
+                }
+                return byteBuffer.toByteArray();
             }
         });
     }
